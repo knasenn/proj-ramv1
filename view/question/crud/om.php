181029dc6e0ac94aa4d7671adcf9b4b103705800
Witold Urbas <aiur18@student.bth.se>
2019-12-24 01:33:37 +0000

    update

--- view/question/crud/om.php
+++ view/question/crud/om.php
@@ -2,5 +2,27 @@
 
 namespace Anax\View;
 
-?><h1>View about</h1>
-<div class="textboxEEE">Hello this is my page. I think its nojs!</div>
+?><h1>Om webbsidan</h1>
+<div class="textboxOM">
+Denna sidan är skapad för kursen ramverk1 på BTH.
+ramverk1, lär ut programmering och objektorienterade kodstrukturer med
+designmönster och modultänkande kring återanvändbara moduler i webbaserade
+ramverk samt tekniker för automatiserad testning, byggsystem och flöde för
+kontinuerlig integration av programvaran.<br>
+<br>
+
+<b>Kurs: </b><a href="https://dbwebb.se/kurser/ramverk1-v2">https://dbwebb.se/kurser/ramverk1-v2</a>
+<br>
+<br>
+Denna sidan är till för att diskutera olika sociala medier samt dess innehåll.
+<br>
+<br>
+<b>Github: </b><a href="https://github.com/knasenn/proj-ramv1">https://github.com/knasenn/proj-ramv1</a>
+
+</div>
+
+<h1>Om mig</h1>
+<div class="textboxOM">
+Hej, jag heter Witold men kallas ibland för Wille.
+Jag är i skrivande stund 36år gamal. Jag är född och uppvuxen i Tyresö strax söder om Stockholm.
+</div>
